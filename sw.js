af36a68f2b828d1cb9ccf8cc6d3167bd71929c5b
todddenison-del <todd.denison@phelps.com>
2026-08-15 07:28:08 -0500

    Upgrade service worker to v1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eefm-shell-v1.0";
+const CACHE_NAME = "eefm-shell-v1.2";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,26 +8,17 @@ const APP_SHELL = [
   "./icon-192.png",
   "./icon-512.png"
 ];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
 });
