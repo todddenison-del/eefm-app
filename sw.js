75179ad7000ead4b5be4d8a1f0550132689133d4
todddenison-del <todd.denison@phelps.com>
2026-08-16 08:32:23 -0500

    EEFM v1.6 final field release

---
+++ sw.js
@@ -0,0 +1,101 @@
+const CACHE_NAME = "eefm-shell-v1.6";
+
+const APP_SHELL = [
+
+  "./",
+
+  "./index.html",
+
+  "./manifest.json",
+
+  "./app.js",
+
+  "./apple-touch-icon.png",
+
+  "./icon-192.png",
+
+  "./icon-512.png"
+
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+      .then(cache => cache.addAll(APP_SHELL))
+
+      .then(() => self.skipWaiting())
+
+  );
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+
+      .then(keys =>
+
+        Promise.all(
+
+          keys
+
+            .filter(k => k !== CACHE_NAME)
+
+            .map(k => caches.delete(k))
+
+        )
+
+      )
+
+      .then(() => self.clients.claim())
+
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+
+    caches.match(event.request).then(cached =>
+
+      cached ||
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+
+            .then(cache => cache.put(event.request, copy))
+
+            .catch(() => {});
+
+          return response;
+
+        })
+
+        .catch(() =>
+
+          event.request.mode === "navigate"
+
+            ? caches.match("./index.html")
+
+            : Response.error()
+
+        )
+
+    )
+
+  );
+
+});
